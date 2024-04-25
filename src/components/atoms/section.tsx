049d1c8119128e45fe57ba69c8b61255fc5b1ddf
Rheno Rayhan <rayhanrheno@gmail.com>
2024-04-26 05:53:16 +0700

    W07: Jawaban Soal 2

--- src/components/atoms/section.tsx
+++ src/components/atoms/section.tsx
@@ -1,7 +1,13 @@
-export default function Section({ children }: { children: any }) {
+"use client";
+
+import { LevelContext } from "@/utilities/context/mycontext";
+
+export default function Section({ level, children }: { level: number, children: any }) {
     return (
         <section className="section">
-            {children}
+            <LevelContext.Provider value={level}>
+                {children}
+            </LevelContext.Provider>
         </section>
     );
 }
