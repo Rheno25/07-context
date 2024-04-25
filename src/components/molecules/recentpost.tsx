e8e4be7e57f822377ed97003bd32e6fd6121fc42
Rheno Rayhan <rayhanrheno@gmail.com>
2024-04-26 06:12:29 +0700

    W07: Jawaban Soal 4

---
+++ src/components/molecules/recentpost.tsx
@@ -0,0 +1,19 @@
+import Heading from "../atoms/heading";
+import Post from "../atoms/post";
+import Section2 from "../atoms/section2";
+
+export default function RecentPosts(){
+    return(
+        <Section2 isFancy = {true}>
+            <Heading>Newest Post</Heading>
+            <Post
+                title="Cita Rasa Lisbon"
+                body="...those pasteis de nata!"
+            />
+            <Post
+                title="Buenos Aires in a tango rhythm"
+                body="I love them!"
+            />
+        </Section2>
+    );
+}
